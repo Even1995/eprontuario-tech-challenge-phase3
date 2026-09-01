6b7a0dba8883c7389f602829d962992d8057e842
Even Cajaiba Silva <66031010+Even1995@users.noreply.github.com>
2026-09-01 20:15:02 -0300

    initial commit

---
+++ src/main/java/com/br/eprontuario/EprontuarioApplication.java
@@ -0,0 +1,13 @@
+package com.br.eprontuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EprontuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EprontuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/eprontuario/EprontuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.eprontuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprontuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
